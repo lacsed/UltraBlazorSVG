b618e24ea9c9fbf6189d6d54be002ef29cfc4c7d
paulocspaes <paulo.c.s.paes@hotmail.com>
2023-10-11 14:35:09 -0300

    Invariant Culture set in Program.cs

--- Program.cs
+++ Program.cs
@@ -4,9 +4,16 @@ using UltraBlazorSVG;
 using System.Globalization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+// Set the culture to InvariantCulture
+CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
+CultureInfo.DefaultThreadCurrentUICulture = CultureInfo.InvariantCulture;
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+Console.WriteLine("feito update");
 
 await builder.Build().RunAsync();
